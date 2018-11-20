0e17ffef8672d0c5b44b620a08327a12dfab15c1
Buckles <doghusky1990@gmail.com>
2018-11-19 22:10:54 -0400

    trying to fix botched targeting in some cases

--- javascript/DomHighlighter.js
+++ javascript/DomHighlighter.js
@@ -8,32 +8,45 @@ $('.element-selector').click(function() {
        e.preventDefault();
    });
 
-   //handle hovering in and out of elements
-   $('body *:not(.tasks-container *)').hover(function() {
-       $(this).css('background-color','yellow')
-
-       //allow the page to be clicked
+   $('body div:last-child:not(.tasks-container *)').each(function() {
+       
+        $(this).hover(function() {
+           
+            //highlight yellow 
+            $(this).css('background-color','yellow');
+           
+        },function() {
+            $(this).css('background-color','')
+        });
+       
        $(this).click(function() {
+            var createdTarget;
+            
+            if ($(this).parent().attr('class')) {
+                createdTarget = $(this).parent().prop('nodeName').toLowerCase()+'.' +$(this).parent().last().attr('class').split(' ').join('.');
+            }else{
+                createdTarget = $(this).parent().prop('nodeName').toLowerCase()+'#' +$(this).parent().last().attr('id');
+            }
+            
+            if ($(this).attr('class')) {
+                createdTarget += " " + $(this).prop('nodeName').toLowerCase()+'.' +$(this).last().attr('class').split(' ').join('.');
+            }else{
+                createdTarget += " " + $(this).prop('nodeName').toLowerCase()+'#' +$(this).last().attr('id');
+            }
+            
+            
+            $('.display-element').html(createdTarget);
+            $('#Form_TaskManagerForm_Element').val(createdTarget);
+            
+            //remove hover effect
+            $('*:not(.tasks-container *)').unbind('mouseenter').unbind('mouseleave')
+            //remove color
+            $('*:not(.tasks-container *)').css('background-color','');
+            $('a').unbind("click");
 
-           //does the element have a class? if not default to id
-           if ($(this).attr('class')) {
-               $('.display-element').html($(this).prop('nodeName').toLowerCase()+'.' +$(this).last().attr('class').split(' ').join('.'));
-               $('#Form_TaskManagerForm_Element').val($(this).prop('nodeName').toLowerCase()+'.' +$(this).last().attr('class').split(' ').join('.'));
-
-           }else{
-               $('.display-element').html($(this).prop('nodeName').toLowerCase()+'#' +$(this).last().attr('id'));
-               $('#Form_TaskManagerForm_Element').val($(this).prop('nodeName').toLowerCase()+'#' +$(this).last().attr('id'));
-           }
-
-           //remove hover effect
-           $('*:not(.tasks-container *)').unbind('mouseenter').unbind('mouseleave')
-           //remove color
-           $('*:not(.tasks-container *)').css('background-color','')
-
-           $('a').unbind("click");
        });
-
-   },function() {
-       $(this).css('background-color','')
+       
    });
+   
+   
 });
