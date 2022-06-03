dd68f39e143ed7b8e80fe5a3b750039dd34c829b
DoggersHusky <robert.ce@webbuildersgroup.com>
2022-06-03 15:37:26 -0300

    more comments

--- src/Model/Milestone.php
+++ src/Model/Milestone.php
@@ -8,6 +8,7 @@ class Milestone extends DataObject
 {
     private static $table_name = 'Milestone';
 
+    //@todo should have a model admin to manage these
     private static $db = [
         'Title' => 'Varchar(255)',
         'Number' => 'Int',
