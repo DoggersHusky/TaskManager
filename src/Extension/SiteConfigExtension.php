a8747df3b465cc55e156926a1d6ef57155aae09b
DoggersHusky <doghusky1990@gmail.com>
2022-06-07 11:11:08 -0300

    formatting

--- src/Extension/SiteConfigExtension.php
+++ src/Extension/SiteConfigExtension.php
@@ -2,9 +2,11 @@
 
 namespace BucklesHusky\TaskManager\Extension;
 
+use SilverStripe\Core\Environment;
 use SilverStripe\Forms\CheckboxField;
 use SilverStripe\Forms\FieldList;
 use SilverStripe\Forms\FormAction;
+use SilverStripe\Forms\LiteralField;
 use SilverStripe\Forms\TextField;
 use SilverStripe\ORM\DataExtension;
 use SilverStripe\SiteConfig\SiteConfig;
@@ -25,34 +27,40 @@ class SiteConfigExtension extends DataExtension
      */
     public function updateCMSFields(FieldList $fields)
     {
-        $fields->addFieldToTab(
-            'Root.Github',
-            CheckboxField::create(
-                'EnableGitIssueCreating',
-                'Enable GitHub features?'
-            )
-            ->setDescription('With this enabled, admins can create issues in this project\'s repo without needing to login to guthub.<br/><strong>Note:</strong> You must have a token set in the ENV file.')
-        );
+        $gitToken = Environment::getEnv('GITHUB_TOKEN');
 
-        $fields->addFieldToTab(
-            'Root.Github',
-            TextField::create('GithubUser', 'GitHub user')
-            ->setDescription('This can be found in the repos url. example: https://github.com/<strong>USER</strong>/REPO/')
-        );
-
-        $fields->addFieldToTab(
-            'Root.Github',
-            CheckboxField::create(
-                'AssignUserToIssue',
-                'Should the user to the issue?'
-            )
-        );
-        
-        $fields->addFieldToTab(
-            'Root.Github',
-            TextField::create('GithubRepo', 'GitHub repo')
-            ->setDescription('This can be found in the repos url. example: https://github.com/USER/<strong>REPO</strong>/')
-        );
+        if ($gitToken) {
+            $fields->addFieldToTab(
+                'Root.Github',
+                CheckboxField::create(
+                    'EnableGitIssueCreating',
+                    'Enable GitHub features?'
+                )
+                ->setDescription('With this enabled, admins can create issues in this project\'s repo without needing to login to guthub.')
+            );
+    
+            $fields->addFieldToTab(
+                'Root.Github',
+                TextField::create('GithubUser', 'GitHub user')
+                ->setDescription('This can be found in the repos url. example: https://github.com/<strong>USER</strong>/REPO/')
+            );
+    
+            $fields->addFieldToTab(
+                'Root.Github',
+                CheckboxField::create(
+                    'AssignUserToIssue',
+                    'Should the user to the issue?'
+                )
+            );
+            
+            $fields->addFieldToTab(
+                'Root.Github',
+                TextField::create('GithubRepo', 'GitHub repo')
+                ->setDescription('This can be found in the repos url. example: https://github.com/USER/<strong>REPO</strong>/')
+            );
+        } else {
+            $fields->addFieldToTab('Root.Github', LiteralField::create('NoGitHub', '<p>You do not have <strong>`GITHUB_TOKEN`</strong> set in your .env</p>'));
+        }
 
         return $fields;
     }
@@ -66,10 +74,9 @@ class SiteConfigExtension extends DataExtension
         $currentSiteConfig = SiteConfig::current_site_config();
 
         if ($currentSiteConfig->EnableGitIssueCreating && $currentSiteConfig->GithubUser && $currentSiteConfig->GithubRepo) {
-
             // add button to update milestones
             $actions->push(
-                    FormAction::create(
+                FormAction::create(
                     'updateMileStones',
                     'Update milestones'
                 )
