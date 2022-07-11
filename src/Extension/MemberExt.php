cdd3d506d1dc6cc42fdcd6ed66a24ea3099cf7fa
DoggersHusky <doghusky1990@gmail.com>
2022-07-11 11:18:27 -0300

    formatting

--- src/Extension/MemberExt.php
+++ src/Extension/MemberExt.php
@@ -5,10 +5,9 @@ namespace BucklesHusky\TaskManager\Extension;
 use SilverStripe\ORM\DataExtension;
 use BucklesHusky\TaskManager\Model\Task;
 
-class MemberExt extends DataExtension {
-    
+class MemberExt extends DataExtension
+{
     private static $has_many = [
-        'Tasks' => Task::class
+        'Tasks' => Task::class,
     ];
-    
 }
